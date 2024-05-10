7f9109023d14a4b45f3301beca3906d1c2bc1179
Azim200117 <kamrunnaherronie@gmail.com>
2024-05-10 23:22:55 +0600

    Folder structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,7 @@
 import './App.css';
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl text-center">Hello Everybody</h1>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
---
+++ src/layout/MainLayout.jsx
@@ -0,0 +1,12 @@
+const MainLayout = () => {
+  return (
+    <div>
+      {/* Navbar */}
+      <h1 className="text-3xl text-center">Hello Everybody</h1>
+      {/* Outlet */}
+      {/* Footer */}
+    </div>
+  );
+};
+
+export default MainLayout;
